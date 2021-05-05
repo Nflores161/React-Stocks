11bc8db27c7612c7352dcdddd5d7d3bf3adac81d
Nick Flores-Tober <nflores161@gmail.com>
2021-05-04 20:53:34 -0400

    Done.

--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -4,12 +4,14 @@ import Stock from '../components/Stock'
 class PortfolioContainer extends Component {
 
   render() {
+
     return (
       <div>
         <h2>My Portfolio</h2>
           {
-            //render your portfolio stocks here
-          }
+            this.props.portfolioStocks.map(stock =>
+            <Stock key={stock.id} stock={stock} tradeStock={this.props.sellStock}/>) 
+          }  
       </div>
     );
   }
--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -4,11 +4,13 @@ import Stock from '../components/Stock'
 class StockContainer extends Component {
 
   render() {
+
     return (
-      <div>
+      <div >
         <h2>Stocks</h2>
         {
-          //render the list of stocks here
+          this.props.stocks.map(stock => 
+          <Stock key={stock.id} stock={stock} tradeStock={this.props.buyStock}/>)
         }
       </div>
     );
